c313e5e7ede56ca27a3611efe34cb9338685b8b1
Deblox <deblox.160999@gmail.com>
2023-04-07 09:47:53 +0200

    add file request sql and fix request

--- the district/src/BDD/requestdistrict.sql
+++ the district/src/BDD/requestdistrict.sql
@@ -3,6 +3,6 @@ ON plat.id_categorie = categorie.id
 JOIN commande
 on command.id_plat = plat.id;
 
-        SELECT *
-        FROM categorie
-        order by id desc
+SELECT *
+FROM categorie
+order by id desc;
