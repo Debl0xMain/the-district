72266e44eccc9e4cf75b15c22ef58c294ff6d803
Deblox <deblox.160999@gmail.com>
2023-05-04 14:51:38 +0200

    add login in user.class

--- the district/src/php/login/loginform.php
+++ the district/src/php/login/loginform.php
@@ -1,29 +1,21 @@
 <?php
-$email_clt = $_POST['mail'];
+include('../class/user.class.php');
+
+$email_clt = 'test@test.com';//$_POST['mail'];
 $password_insc = password_hash($_POST['password'], PASSWORD_DEFAULT);
-$password_clt = $_POST['password'];
-echo $password_clt;
-$email_bdd[0] = 'tesdt@gmail.com';
-$email_bdd[1] = 'test@gmail.com';
-$password_bdd = '$2y$10$OgqjPHtZ3R4i3zm0Dos1kOzepbQf/NzwpPvy4nlz2YgwWLS6PL2Qq';
+$password_clt = 'test';//$_POST['password'];
+$name = 't';
+$email = 'test@test.com';
+$password = '$2y$10$OgqjPHtZ3R4i3zm0Dos1kOzepbQf/NzwpPvy4nlz2YgwWLS6PL2Qq';
+$command = 't';
+$imgprofil = 't';
+$rank = 't';
+$objet = new _user($name, $email, $password, $command, $imgprofil, $rank);
 
-if (in_array($email_clt,$email_bdd)){
-    echo 'email valide';
-    if (password_verify($password_clt,$password_bdd) === true) {
-        $login = 'User';
-        echo 'mdp valide';
-        session_start($login);
-        setcookie($login);
-    }
-    else {
-        echo 'mdp non valide';
-    }
-}
-else {
-    echo 'email non valide';
-}
+$emailcheck = $objet->_email;
+$passcheck = $objet->_password;
 
-
+$objet->checklogin($email_clt,$password_clt,$passcheck,$emailcheck);
 
 
 ?>
