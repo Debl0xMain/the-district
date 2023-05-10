1e9c892d0026f881237d235953f148978357ebed
Deblox <deblox.160999@gmail.com>
2023-05-10 10:10:46 +0200

    login work

--- the district/src/php/class/user.class.php
+++ the district/src/php/class/user.class.php
@@ -5,15 +5,13 @@ class _user
     public $_name;
     public $_email;
     public $_password;
-    public $_command;
     public $_imgprofil;
     public $_rank;
 
-     public function __construct ($name, $email, $password, $command, $imgprofil, $rank) {
+     public function __construct ($name, $email, $password, $imgprofil, $rank) {
         $this->_name = $name;
         $this->_email = $email;
         $this->_password = $password;
-        $this->_command =  $command;
         $this->_imgprofil = $imgprofil;
         $this->_rank = $rank;
     }
--- the district/src/php/login/loginform.php
+++ the district/src/php/login/loginform.php
@@ -1,16 +1,11 @@
 <?php
 include('../class/user.class.php');
 
-$email_clt = 'test@test.com';//$_POST['mail'];
+$email_clt = $_POST['mail'];
 $password_insc = password_hash($_POST['password'], PASSWORD_DEFAULT);
-$password_clt = 'test';//$_POST['password'];
-$name = 't';
-$email = 'test@test.com';
-$password = '$2y$10$OgqjPHtZ3R4i3zm0Dos1kOzepbQf/NzwpPvy4nlz2YgwWLS6PL2Qq';
-$command = 't';
-$imgprofil = 't';
-$rank = 't';
-$objet = new _user($name, $email, $password, $command, $imgprofil, $rank);
+$password_clt = $_POST['password'];
+
+$objet = new _user($name, $email, $password, $imgprofil, $rank);
 
 $emailcheck = $objet->_email;
 $passcheck = $objet->_password;
