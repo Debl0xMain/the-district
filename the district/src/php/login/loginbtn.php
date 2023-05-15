3fe857d39c8dee322ed64799689c5bd622771f64
Deblox <deblox.160999@gmail.com>
2023-05-15 15:52:54 +0200

    fix

--- the district/src/php/login/loginbtn.php
+++ the district/src/php/login/loginbtn.php
@@ -5,7 +5,7 @@ echo
 <div class="position-fixed btnlogin">
 <button type="button" class="btn btn btn-outline-light btnicon" id="logincharge" data-bs-toggle="modal" data-bs-target="#login">
 <i class="fa-solid fa-user"></i>
- <p class="btnlogintext"> Login </p>
+ <p class="btnlogintext" id="btnlogintext"> Login </p>
 
 </button>
 </div>
